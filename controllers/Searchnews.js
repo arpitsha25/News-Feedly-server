81681c71c98d3b723a20f38833a86ef1ff02816a
arpitsha25 <arpit.sha25@gmail.com>
2023-08-14 19:17:35 +0530

    commit

--- controllers/Searchnews.js
+++ controllers/Searchnews.js
@@ -1,12 +1,14 @@
 // const mongoose = require("mongoose");
-
+const Newsschema = require("../modals/newsmodal");
 const Searchnews = (req, res) => {
   const item = req.body.searchitem;
   let searched;
   async function search() {
-    searched = await User.findOne({ email: req.body.email });
+    searched = await Newsschema.find({
+      title: { $regex: item, $options: "i" }, // Case-insensitive search
+    });
     if (searched) {
-      res.status(200).send({ msg: "success" });
+      res.status(200).send({ msg: "success", data: searched });
     } else {
       res.status(404).send({ msg: "No news found" });
     }
